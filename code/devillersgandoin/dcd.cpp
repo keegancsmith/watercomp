272a3e68a8e600735703af91c487a1ed418c95e8
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2009-08-14 01:39:34 +0200

    Implemented decompressing a compressed DCD file using Devillers and Gandoin. Currently does not write out a DCD file.

--- code/devillersgandoin/dcd.cpp
+++ code/devillersgandoin/dcd.cpp
@@ -54,7 +54,7 @@ void compress(string dcd_path, string out_path, int quantisation_bits=10) {
     for (int i = 0; i < dcd->nsets; ++i)
     {
         assert(!read_next_timestep(dcd, natoms, &timestep));
-        printf("Processing Frame %d of %d...", i, dcd->nsets);
+        printf("Processing Frame %d of %d...", i+1, dcd->nsets);
 
         // Bounding box
         float min_box[3], max_box[3];
@@ -119,11 +119,110 @@ void compress(string dcd_path, string out_path, int quantisation_bits=10) {
     ac_encoder_done(&encoder);
     ac_model_done(&model);
     fclose(fout);
+    delete[] timestep.coords;
+}
+
+
+void decompress(string in_path, string out_path) {
+    ac_decoder decoder;
+    ac_model model;
+    FILE * fin;
+    float * points;
+
+
+    // Setup arithmetic decoders to use in_path
+    fin = fopen(in_path.c_str(), "rb");
+    ac_decoder_init(&decoder, fin);
+    ac_model_init(&model, 256, NULL, 1);
+
+
+    // Read global header
+    int header[3];
+    ac_decode_data(header, sizeof(int), 3, &decoder, &model);
+    int quantisation_bits = header[0];
+    int natoms            = header[1];
+    int nframes           = header[2];
+
+
+    // Stores points for each frame before outputing
+    points = new float[3 * natoms];
+
+
+    // Process each frame and write out
+    for (int i = 0; i < nframes; ++i)
+    {
+        printf("Processing Frame %d of %d...", i+1, nframes);
+
+        // Read frame header
+        float min_box[3], max_box[3];
+        unsigned int size;
+        ac_decode_data(min_box, sizeof(float), 3, &decoder, &model);
+        ac_decode_data(max_box, sizeof(float), 3, &decoder, &model);
+        ac_decode_data(&size,   sizeof(unsigned int), 1, &decoder, &model);
+
+        float range[3];
+        for (int d = 0; d < 3; d++)
+            range[d] = max_box[d] - min_box[d];
+
+
+        // TODO read in points permutation
+
+
+        // Read in encoded data
+        vector<coord_t> data;
+        for (size_t j = 0; j < size; j++) {
+            coord_t c;
+            ac_decode_data(&c, sizeof(coord_t), 1, &decoder, &model);
+            data.push_back(c);
+        }
+
+
+        // Do the reverse devillers and gandoin transformation
+        vector<point_t> decoded = decode(data, quantisation_bits);
+
+
+        // Approximate quantised points with floats
+        for (size_t j = 0; j < decoded.size(); j++) {
+            float buckets = 1 << quantisation_bits;
+
+            for (int d = 0; d < 3; d++) {
+                float approx = decoded[j].coords[d] + 0.5;
+                points[3*j+d] = approx * range[d] / buckets;
+            }
+        }
+
+        // TODO write points to file
+
+        printf("Done\n");
+    }
+
+
+    // Cleanup
+    ac_decoder_done(&decoder);
+    ac_model_done(&model);
+    fclose(fin);
+    delete[] points;
+}
+
+
+int usage() {
+    printf("USAGE: ./dcd (c|d) in out\n");
 }
 
 
 int main(int argc, char ** argv) {
-    assert(argc == 3);
-    compress(argv[1], argv[2]);
+    assert(argc == 4);
+
+    string cmd = argv[1];
+
+    if (cmd == "c")
+        compress(argv[2], argv[3]);
+    else if (cmd == "d")
+        decompress(argv[2], argv[3]);
+    else {
+        usage();
+        return 1;
+    }
+
     return 0;
 }
