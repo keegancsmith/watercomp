4c728d20740554138c0bd0fd17958b5ca7972713
Min-Young Wu <mwu368@gmail.com>
2009-09-19 22:04:13 +0200

    Marching tetrahedrons added, needs fixing.

--- code/visualisation/inc/renderer.h
+++ code/visualisation/inc/renderer.h
@@ -3,9 +3,28 @@
 
 #include <QGLWidget>
 #include <QTimer>
+#include <QVector>
 
 class Frame_Data;
 class Quaternion;
+
+struct Point3f
+{
+    float p[3];
+};//Point3f
+
+struct Triangle
+{
+    Point3f p[3]; // 3 positions
+    Point3f n[3]; // 3 normals
+};//Triangle
+
+struct GridCell
+{
+    Point3f p[8]; // 8 positions
+    Point3f n[8]; // 8 normals
+    float val[8]; // 8 values
+};//GridCell
 
 class Renderer : public QGLWidget
 {
@@ -66,6 +85,13 @@ class Renderer : public QGLWidget
 
         Quaternion* rot;
         Frame_Data* data;
+
+        QVector<Triangle> _surface;
+        void fillGridCell(GridCell& grid, unsigned char*** data, int i, int x, int y, int z);
+        void marchTetrahedron(QVector<Triangle>& surface, GridCell& grid, int iso, int v0, int v1, int v2, int v3);
+        Point3f vertexInterpolate(float iso, GridCell& g, int v1, int v2);
+        void initMetaballs();
+        void tickMetaballs();
 
         void renderAxes();
         void renderPoints();
