6261ecc50085d4934f07332d51fca632ee9e9ed3
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2009-10-09 05:05:35 +0200

    Made SpanningTree use KDTree. Currently very slow due to slow heap operations.

--- code/components/graph/KDTree.h
+++ code/components/graph/KDTree.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include <queue>
+#include <algorithm>
 
 class KDTree
 {
@@ -26,3 +28,67 @@ private:
     KDTree * m_left;
     KDTree * m_right;
 };
+
+
+// Below has to be here otherwise linker errors galore
+class SearchCmp
+{
+public:
+    SearchCmp(int x, int y, int z) {
+        v[0] = x;
+        v[1] = y;
+        v[2] = z;
+    }
+
+    // manhattan dist
+    int dist_from_v(const KDTree * node) const {
+        int d = 0;
+        for (int i = 0; i < 3; i++) {
+            int a = v[i] - node->m_min_box[i];
+            int b = v[i] - node->m_max_box[i];
+            if (a < 0)
+                a *= -1;
+            if (b < 0)
+                b *= -1;
+            d += std::min(a, b);
+        }
+        return d;
+    }
+
+    bool operator ()(const KDTree*a, const KDTree*b) const {
+        int a_dist = dist_from_v(a);
+        int b_dist = dist_from_v(b);
+        // reverse of what you expect cause want minheap
+        return b_dist < a_dist;
+    }
+
+    int v[3];
+};
+
+
+template<class Predicate>
+void KDTree::iterate_closest(int x, int y, int z, Predicate & pred)
+{
+    SearchCmp cmp(x, y, z);
+    std::priority_queue<KDTree*, std::vector<KDTree*>, SearchCmp> q(cmp);
+
+    q.push(this);
+
+    while (!q.empty()) {
+        KDTree * node = q.top();
+        q.pop();
+
+        // If we are at a leaf check if we can stop searching
+        if (node->m_upper - node->m_lower == 1) {
+            bool finish = pred(m_index->at(node->m_lower));
+            if (finish)
+                return;
+            else
+                continue;
+        }
+
+        // Add children
+        q.push(node->m_left);
+        q.push(node->m_right);
+    }
+}
--- code/components/graph/SpanningTree.cpp
+++ code/components/graph/SpanningTree.cpp
@@ -1,47 +1,45 @@
 #include "SpanningTree.h"
+#include "KDTree.h"
 
-#include <vector>
 #include <climits>
+#include <functional>
 #include <queue>
+#include <vector>
 
 using std::vector;
 using std::queue;
 
 
-int dist_squared(const QuantisedFrame & frame, int a, int b)
-{
-    int d = 0;
-    for (int i = 0; i < 3; i++) {
-        int x = frame.quantised_frame[a*3 + i];
-        int y = frame.quantised_frame[b*3 + i];
-        int delta = x - y;
-        d += (delta * delta);
-    }
-    return d;
-}
-
 // Find closest unseen atom
-int find_closest_atom(const QuantisedFrame & frame, vector<bool> & seen,
-                      int v)
+class FindAtoms
 {
-    int closest = v == 0 ? 1 : 0;
-    int closest_dist = INT_MAX;
-    for (size_t e = 0; e < frame.natoms(); e++) {
-        if (e == v || seen[e])
-            continue;
-        int d = dist_squared(frame, v, e);
-        if (d < closest_dist) {
-            closest = e;
-            closest_dist = d;
-        }
+public:
+    FindAtoms(const QuantisedFrame * frame, vector<bool> * seen, size_t k)
+        : frame(frame), seen(seen), k(k) {}
+
+    bool operator() (int i) {
+        if (!seen->at(i))
+            atoms_idx.push_back(i);
+        return atoms_idx.size() == k;
     }
 
-    return closest;
-}
+    const QuantisedFrame * frame;
+    vector<bool> * seen;
+    size_t k;
+    vector<int> atoms_idx;
+};
 
 Graph * spanning_tree(const QuantisedFrame & frame, int & root) {
     vector<bool> seen;
     seen.resize(frame.natoms());
+
+    // Create KD-Tree
+    vector<unsigned int> points(frame.quantised_frame);
+    vector<int> index;
+    index.resize(frame.natoms());
+    for (size_t i = 0; i < index.size(); i++)
+        index[i] = i;
+    KDTree search_tree(&points, &index);
 
     Graph * tree = new Graph(&frame, frame.natoms());
 
@@ -55,10 +53,16 @@ Graph * spanning_tree(const QuantisedFrame & frame, int & root) {
         int v = q.front();
         q.pop();
 
-        for (int i = 0; i < 2; i++) {
-            int k = find_closest_atom(frame, seen, v);
-            if (seen[k])
-                break;
+        // Find 2 closest unseen atoms
+        FindAtoms fa(&frame, &seen, 2);
+        search_tree.iterate_closest(frame.quantised_frame[v*3 + 0],
+                                    frame.quantised_frame[v*3 + 1],
+                                    frame.quantised_frame[v*3 + 2],
+                                    fa);
+
+        // Add the unseen atoms to the queue
+        for (size_t i = 0; i < fa.atoms_idx.size(); i++) {
+            int k = fa.atoms_idx[i];
             seen[k] = true;
             tree->addEdge(v, k);
             q.push(k);
