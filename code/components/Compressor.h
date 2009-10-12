696c70a64324a03b34d3caa3a8a469757236156d
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2009-10-12 16:53:05 +0200

    Made the compressor program optionally handle PDB files. Improved error reporting.

--- code/components/Compressor.h
+++ code/components/Compressor.h
@@ -2,14 +2,20 @@
 
 #include "FrameReader.h"
 #include "FrameWriter.h"
+#include "pdbio/AtomInformation.h"
 
 #include <cstdio>
+#include <vector>
 
 class Compressor
 {
 public:
     virtual FrameReader * frame_reader(FILE * fin) = 0;
     virtual FrameWriter * frame_writer(FILE * fout) = 0;
+
+    virtual bool needs_atom_information() const { return false; }
+
+    std::vector<AtomInformation> m_atom_information;
 };
 
 
