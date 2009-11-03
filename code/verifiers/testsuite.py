2af1faf6872e7ea0dbaa38dd2002f4eec7eb331b
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2009-11-03 02:42:36 +0200

    Made testsuite much more advanced.

--- code/verifiers/testsuite.py
+++ code/verifiers/testsuite.py
@@ -1,88 +1,61 @@
 #!/usr/bin/env python
 
+import cPickle as pickle
+import glob
+import itertools
 import os
 import os.path
+import pprint
 import sys
+import time
 
+RESULTS      = os.path.expanduser('~/dcd_results')
+QUANTISATION = '8 12 16'.split()
 PERMUTATIONS = 'null best naive interframe delta'.split()
-DCDS = 'mscl smallwater mscl1000atoms mscl5frames gala_pmf'.split()
-
-CMD_PATH = os.path.abspath(sys.argv[0])
-WORKING_DIR = os.path.dirname(os.path.dirname(CMD_PATH))
-OUTPUT_DIR = os.path.expanduser('~/dcd_output')
+CMD_PATH     = os.path.abspath(sys.argv[0])
+WORKING_DIR  = os.path.dirname(os.path.dirname(CMD_PATH))
+INPUT_DIR    = os.path.expanduser('~/dcd_input')
+DCDS         = [os.path.splitext(os.path.basename(a))[0]
+                for a in glob.glob(INPUT_DIR + '/*.dcd')]
 
 TESTS = {
-    'common_interframe': {
-        'bin'    : './common_interframe/driver',
-        'dcds'   : DCDS,
-        'author' : 'julian',
-    },
-
     'dg': {
         'bin'    : './devillersgandoin/dgcomp',
         'perms'  : PERMUTATIONS,
-        'dcds'   : DCDS,
         'author' : 'keegan',
-    },
-
-    'interframe': {
-        'bin'    : './interframe/driver',
-        'dcds'   : DCDS,
-        'author' : 'julian',
-    },
-
-    'nearest_interframe': {
-        'bin'    : './nearest_interframe/driver',
-        'dcds'   : DCDS,
-        'author' : 'julian',
-    },
-
-    'nn_interframe': {
-        'bin'    : './nn_interframe/driver',
-        'dcds'   : DCDS,
-        'author' : 'julian',
-    },
-
-    'spline_interframe': {
-        'bin'    : './spline_interframe/driver',
-        'dcds'   : DCDS,
-        'author' : 'julian',
     },
 
     'gumhold': {
         'bin'    : './gumhold/gumhold',
         'perms'  : PERMUTATIONS,
-        'dcds'   : DCDS,
         'author' : 'keegan',
     },
 
-    'omeltchenko': {
-        'bin'    : './omeltchenko/driver',
-        'dcds'   : DCDS,
-        'author' : 'julian',
-    },
+    # 'omeltchenko': {
+    #     'bin'    : './omeltchenko/driver',
+    #     'author' : 'julian',
+    # },
 
     'quant': {
         'bin'    : './quantcomp/quantcomp',
-        'dcds'   : DCDS,
         'author' : 'keegan',
     },
 
-    'bzip2': {
-        'bin'    : './quantcomp/bzip2comp',
-        'dcds'   : DCDS,
+    'gzip': {
+        'bin'    : './quantcomp/gzipcomp',
         'author' : 'keegan',
     },
 
     'watercomp': {
         'bin'    : './watercomp/watercomp',
-        'dcds'   : DCDS,
         'perms'  : PERMUTATIONS,
         'author' : 'keegan',
     },
 }
 
 
+# If a test supports permutations, add all versions of the test with different
+# permutations
 TESTS_ALL = {}
 for k, v in TESTS.items():
     TESTS_ALL[k] = v
@@ -92,14 +65,35 @@ for k, v in TESTS.items():
         for p in v['perms']:
             test = {
                 'bin'    : 'PERMUTATION="%s" %s' % (p, v['bin']),
-                'dcds'   : v['dcds'],
                 'author' : v['author'],
             }
             TESTS_ALL['%s_%s' % (k,p)] = test
 
 
+def get_results():
+    f = open(RESULTS + '.dat')
+    r = pickle.load(f)
+    f.close()
+    return r
+
+def write_results(r):
+    f = open(RESULTS + '.dat', 'w')
+    pickle.dump(r, f)
+    f.close()
+    f = open(RESULTS + '.txt', 'w')
+    pprint.pprint(r, f, 4)
+    f.close()
+
+def write_result(dcd, quant, comp, size, _time):
+    r = get_results()
+    r[(dcd, quant, comp)] = (size, _time)
+    write_results(r)
+
+if not os.path.exists(RESULTS + '.dat'):
+    write_results({})
+
 if __name__ == '__main__':
-    valid_tests = sorted(TESTS_ALL.keys() + ['all'])
+    valid_tests = sorted(TESTS_ALL.keys() + ['all', 'untested'])
     tests = sys.argv[1:]
 
     # Parse args
@@ -108,6 +102,12 @@ if __name__ == '__main__':
         tests.remove('verify')
     else:
         verify = False
+
+    if 'untested' in tests:
+        untested = True
+        tests.remove('untested')
+    else:
+        untested = False
 
     if 'quiet' in tests:
         quiet = lambda x : x + ' > /dev/null'
@@ -122,6 +122,7 @@ if __name__ == '__main__':
         tests = tests[:i]
     else:
         dcds = DCDS
+    dcds = sorted(dcds)
 
     if not tests or not set(tests).issubset(valid_tests):
         print "Valid tests are:\n * " + '\n * '.join(valid_tests)
@@ -140,15 +141,12 @@ if __name__ == '__main__':
             tests_run.append('%s_%s' % (test, p))
     tests_run = sorted(set(tests_run))
 
-    # Setup Dirs
     os.chdir(WORKING_DIR)
-    if not os.path.exists(OUTPUT_DIR):
-        os.mkdir(OUTPUT_DIR)
-
+    tested = set(get_results().keys())
     verify_failed = []
 
     # Run each test
-    for name in tests_run:
+    for name, dcd, quant in itertools.product(tests_run, dcds, QUANTISATION):
         test = TESTS_ALL[name]
 
         if test['author'] is 'keegan':
@@ -156,42 +154,53 @@ if __name__ == '__main__':
         else:
             cflag, dflag = 'c', 'x'
 
-        print '='*len(name)
-        print name
-        print '='*len(name)
+        if untested and (dcd, quant, name) in tested:
+            continue
 
-        for dcd in test['dcds']:
-            if dcd not in dcds:
-                continue
+        tag = ' '.join([dcd, quant, name])
+        print '\n', '*' * len(tag)
+        print tag
+        print '*' * len(tag), '\n'
 
-            print '\n', dcd
+        pdb_path = '%s/%s.pdb' % (INPUT_DIR, dcd)
+        dcd_path = '%s/%s.dcd' % (INPUT_DIR, dcd)
+        cmp_path = '/tmp/foo.cmp'
+        dec_path = '/tmp/foo.dcd'
 
-            pdb_path = '../dcd/%s.pdb' % dcd
-            dcd_path = '../dcd/%s.dcd' % dcd
-            cmp_path = os.path.join(OUTPUT_DIR, '%s_%s.cmp' % (dcd, name))
-            dec_path = os.path.join(OUTPUT_DIR, '%s_%s.dcd' % (dcd, name))
+        flags = ['-q ' + quant]
+        if os.path.exists(pdb_path):
+            flags.append('--pdb ' + pdb_path)
+        flags = ' '.join(flags)
 
-            flags = []
-            if os.path.exists(pdb_path):
-                flags.append('--pdb ' + pdb_path)
-            flags = ' '.join(flags)
+        cmp_cmd = '%s %s %s %s %s' % (test['bin'], cflag, flags,
+                                      dcd_path, cmp_path)
 
-            cmp_cmd = '%s %s %s %s %s' % (test['bin'], cflag, flags,
-                                          dcd_path, cmp_path)
-            os.system(quiet(cmp_cmd))
+        t = time.time()
+        os.system(quiet(cmp_cmd))
+        t = time.time() - t
+        s = os.stat(cmp_path).st_size
 
-            if not verify:
-                continue
+        # Give 5 seconds for the user to cancel
+        try:
+            time.sleep(5)
+        except:
+            sys.exit(0)
 
-            dec_cmd = '%s %s %s %s %s' % (test['bin'], dflag, flags,
-                                          cmp_path, dec_path)
-            os.system(quiet(dec_cmd))
+        write_result(dcd, quant, name, s, t)
 
-            check_cmd = './verifiers/checkdcd %s %s' % (dcd_path, dec_path)
-            verified = os.system(check_cmd)
+        if not verify:
+            continue
 
-            if verified != 0:
-                verify_failed.append('%s_%s' % (dcd, name))
+        dec_cmd = '%s %s %s %s %s' % (test['bin'], dflag, flags,
+                                      cmp_path, dec_path)
+        os.system(quiet(dec_cmd))
+
+        check_cmd = './verifiers/checkdcd %s %s' % (dcd_path, dec_path)
+        verified = os.system(check_cmd)
+
+        if verified != 0:
+            verify_failed.append('%s_%s' % (dcd, name))
+
         print
 
     if verify:
