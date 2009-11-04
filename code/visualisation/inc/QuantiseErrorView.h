a7861aa50864ff6468e81cf6fe094ee3f8597152
Min-Young Wu <mwu368@gmail.com>
2009-11-04 20:22:57 +0200

    Some more config values for QuantiseErrorView.

--- code/visualisation/inc/QuantiseErrorView.h
+++ code/visualisation/inc/QuantiseErrorView.h
@@ -5,7 +5,9 @@
 
 #include <pdbio/Frame.h>
 
+class QCheckBox;
 class QDoubleSpinBox;
+class QLabel;
 class QSettings;
 class QSlider;
 
@@ -27,6 +29,8 @@ class QuantiseErrorView : public BaseView
         virtual void setupPreferenceWidget(QWidget* preferenceWidget);
 
     private slots:
+        void setDrawLine(int state);
+        void setPointSize(int value);
         void setLineSize(int value);
         void setStartErrorValue(double value);
         void setFinalErrorValue(double value);
@@ -34,21 +38,29 @@ class QuantiseErrorView : public BaseView
         void pickFinalColor();
         void setStartAlpha(int value);
         void setFinalAlpha(int value);
+        void setThresholdValue(int value);
 
     private:
         QSettings* settings;
         float* errors;
         int _lineSize;
+        int _pointSize;
         float _startColor[4];
         float _finalColor[4];
         double _startErrorValue;
         double _finalErrorValue;
+        double _thresholdValue;
+        bool _drawLine;
 
+        QSlider* pointSizeSlider;
         QSlider* lineSizeSlider;
         QSlider* startAlphaSlider;
         QSlider* finalAlphaSlider;
         QDoubleSpinBox* startErrorSpinBox;
         QDoubleSpinBox* finalErrorSpinBox;
+        QSlider* thresholdSlider;
+        QLabel* maxErrorLabel;
+        QCheckBox* drawLineCheckBox;
 
 };//QuantiseErrorView
 
--- code/visualisation/src/QuantiseErrorView.cpp
+++ code/visualisation/src/QuantiseErrorView.cpp
@@ -4,12 +4,14 @@
 #include <cstdio>
 #include <GL/gl.h>
 
+#include <QCheckBox>
 #include <QDoubleSpinBox>
 #include <QGridLayout>
 #include <QLabel>
 #include <QPushButton>
 #include <QSettings>
 #include <QSlider>
+#include <QSpacerItem>
 #include <QWidget>
 
 #include <quantiser/QuantisedFrame.h>
@@ -18,6 +20,8 @@
 
 #define MAX_ALPHA_SLIDER 100
 #define MAX_ALPHA_VAL 1.0
+
+#define MAX_POINT_SIZE 20
 
 #define MAX_LINE_WIDTH 10
 
@@ -36,9 +40,12 @@ QuantiseErrorView::QuantiseErrorView()
     _finalColor[2] = settings->value("QuantiseErrorView/finalColorB", 0.0).toDouble();
     _finalColor[3] = settings->value("QuantiseErrorView/finalColorA", 1.0).toDouble();
     _preferenceWidget = NULL;
+    _pointSize = settings->value("QuantiseErrorView/pointSize", 10).toInt();
     _lineSize = settings->value("QuantiseErrorView/lineSize", 2).toInt();
     _startErrorValue = settings->value("QuantiseErrorView/startError", 0.06).toDouble();
     _finalErrorValue = settings->value("QuantiseErrorView/finalError", 0.06).toDouble();
+    _thresholdValue = settings->value("QuantiseErrorView/thresholdValue", 0.5).toDouble();
+    _drawLine = settings->value("QuantiseErrorView/drawLine", true).toBool();
     errors = 0;
     doSplitWaters = true;
 }//constructor
@@ -52,11 +59,14 @@ QuantiseErrorView::~QuantiseErrorView()
 
 void QuantiseErrorView::updatePreferences()
 {
+    pointSizeSlider->setValue(_pointSize);
     lineSizeSlider->setValue(_lineSize);
     startAlphaSlider->setValue((int)(_startColor[3] * MAX_ALPHA_SLIDER / MAX_ALPHA_VAL));
     finalAlphaSlider->setValue((int)(_finalColor[3] * MAX_ALPHA_SLIDER / MAX_ALPHA_VAL));
     startErrorSpinBox->setValue(_startErrorValue);
     finalErrorSpinBox->setValue(_finalErrorValue);
+    thresholdSlider->setValue(_thresholdValue * 100);
+    drawLineCheckBox->setCheckState(_drawLine ? Qt::Checked : Qt::Unchecked);
 }//updatePreferences
 
 void QuantiseErrorView::setupPreferenceWidget(QWidget* preferenceWidget)
@@ -109,14 +119,47 @@ void QuantiseErrorView::setupPreferenceWidget(QWidget* preferenceWidget)
     connect(finalAlphaSlider, SIGNAL(valueChanged(int)), this, SLOT(setFinalAlpha(int)));
     layout->addWidget(finalAlphaSlider, 5, 1);
 
+    layout->addItem(new QSpacerItem(6, 24), 7, 0);
+
+    QLabel* drawLineLabel = new QLabel(tr("Draw line"), preferenceWidget);
+    layout->addWidget(drawLineLabel, 7, 0);
+
+    drawLineCheckBox = new QCheckBox(preferenceWidget);
+    connect(drawLineCheckBox, SIGNAL(stateChanged(int)), this, SLOT(setDrawLine(int)));
+    layout->addWidget(drawLineCheckBox, 7, 1);
+
+    QLabel* pointSizeLabel = new QLabel(tr("Point size"), preferenceWidget);
+    layout->addWidget(pointSizeLabel, 8, 0);
+
+    pointSizeSlider = new QSlider(preferenceWidget);
+    pointSizeSlider->setOrientation(Qt::Horizontal);
+    pointSizeSlider->setRange(0, MAX_POINT_SIZE);
+    connect(pointSizeSlider, SIGNAL(valueChanged(int)), this, SLOT(setPointSize(int)));
+    layout->addWidget(pointSizeSlider, 8, 1);
+
     QLabel* lineSizeLabel = new QLabel(tr("Line width"), preferenceWidget);
-    layout->addWidget(lineSizeLabel, 6, 0);
+    layout->addWidget(lineSizeLabel, 9, 0);
 
     lineSizeSlider = new QSlider(preferenceWidget);
     lineSizeSlider->setOrientation(Qt::Horizontal);
     lineSizeSlider->setRange(0, MAX_LINE_WIDTH);
     connect(lineSizeSlider, SIGNAL(valueChanged(int)), this, SLOT(setLineSize(int)));
-    layout->addWidget(lineSizeSlider, 6, 1);
+    layout->addWidget(lineSizeSlider, 9, 1);
+
+    QLabel* thresholdLabel = new QLabel(tr("Middle threshold value"), preferenceWidget);
+    layout->addWidget(thresholdLabel, 10, 0);
+
+    thresholdSlider = new QSlider(preferenceWidget);
+    thresholdSlider->setOrientation(Qt::Horizontal);
+    thresholdSlider->setRange(0, 100);
+    connect(thresholdSlider, SIGNAL(valueChanged(int)), this, SLOT(setThresholdValue(int)));
+    layout->addWidget(thresholdSlider, 10, 1);
+
+    QLabel* maxErrorLabelLabel = new QLabel(tr("Maximum error"), preferenceWidget);
+    layout->addWidget(maxErrorLabelLabel, 11, 0);
+
+    maxErrorLabel = new QLabel(tr("0"), preferenceWidget);
+    layout->addWidget(maxErrorLabel, 11, 1);
 
     preferenceWidget->setLayout(layout);
 }//setupPreferenceWidget
@@ -142,48 +185,94 @@ void QuantiseErrorView::tick(int framenum, Frame* unquantised, QuantisedFrame* q
     float range = _finalErrorValue - _startErrorValue;
     if (errors) delete errors;
     errors = new float[waters.size()];
+    float maxError = 0;
     for (int i = 0; i < waters.size(); i++)
     {
         dif[0] = dequantised->atom_data[3*waters[i].OH2_index  ] - unquantised->atom_data[3*waters[i].OH2_index];
         dif[1] = dequantised->atom_data[3*waters[i].OH2_index+1] - unquantised->atom_data[3*waters[i].OH2_index+1];
         dif[2] = dequantised->atom_data[3*waters[i].OH2_index+2] - unquantised->atom_data[3*waters[i].OH2_index+2];
         d = sqrt(len2(dif));
+        if (d > maxError) maxError = d;
         errors[i] = (d - _startErrorValue) / range;
         if (errors[i] < 0) errors[i] = 0;
         if (errors[i] > 1) errors[i] = 1;
-        if (errors[i] > 0.5) errors[i] = 1;
+        if (errors[i] > _thresholdValue) errors[i] = 1;
     }//for
+    maxErrorLabel->setNum(maxError);
+    if (maxError > finalErrorSpinBox->maximum())
+        finalErrorSpinBox->setMaximum(maxError);
 }//tick
 
 void QuantiseErrorView::render()
 {
-    if (dequantised == NULL) return;
+    Frame* data = drawQuantised ? dequantised : unquantised;
+    if (data == NULL) return;
 
     //draw points
     float range[] = {_finalColor[0] - _startColor[0],
                      _finalColor[1] - _startColor[1],
                      _finalColor[2] - _startColor[2],
                      _finalColor[3] - _startColor[3]};
-    glBegin(GL_LINES);
-    for (int i = 0; i < waters.size(); i++)
+    if (_drawLine)
     {
-        if (errors[i] > 1e-6)
+        glBegin(GL_LINES);
+        for (int i = 0; i < waters.size(); i++)
         {
-            glColor4f(_startColor[0] + errors[i] * range[0],
-                      _startColor[1] + errors[i] * range[1],
-                      _startColor[2] + errors[i] * range[2],
-                      _startColor[3] + errors[i] * range[3]);
-            glVertex3f(unquantised->atom_data[3*waters[i].OH2_index],
-                       unquantised->atom_data[3*waters[i].OH2_index+1],
-                       unquantised->atom_data[3*waters[i].OH2_index+2]);
-            glVertex3f(dequantised->atom_data[3*waters[i].OH2_index],
-                       dequantised->atom_data[3*waters[i].OH2_index+1],
-                       dequantised->atom_data[3*waters[i].OH2_index+2]);
-        }//if
-    }//for
-    glEnd();
+            if (errors[i] > 1e-6)
+            {
+                glColor4f(_startColor[0] + errors[i] * range[0],
+                        _startColor[1] + errors[i] * range[1],
+                        _startColor[2] + errors[i] * range[2],
+                        _startColor[3] + errors[i] * range[3]);
+                glVertex3f(unquantised->atom_data[3*waters[i].OH2_index],
+                        unquantised->atom_data[3*waters[i].OH2_index+1],
+                        unquantised->atom_data[3*waters[i].OH2_index+2]);
+                glVertex3f(dequantised->atom_data[3*waters[i].OH2_index],
+                        dequantised->atom_data[3*waters[i].OH2_index+1],
+                        dequantised->atom_data[3*waters[i].OH2_index+2]);
+            }//if
+        }//for
+        glEnd();
+    }//if
+    else
+    {
+        glBegin(GL_POINTS);
+        for (int i = 0; i < waters.size(); i++)
+        {
+            if (errors[i] > 1e-6)
+            {
+                glColor4f(_startColor[0] + errors[i] * range[0],
+                          _startColor[1] + errors[i] * range[1],
+                          _startColor[2] + errors[i] * range[2],
+                          _startColor[3] + errors[i] * range[3]);
+                glVertex3f(data->atom_data[3*waters[i].OH2_index],
+                           data->atom_data[3*waters[i].OH2_index+1],
+                           data->atom_data[3*waters[i].OH2_index+2]);
+            }//if
+        }//for
+        glEnd();
+    }//else
 }//render
 
+
+void QuantiseErrorView::setDrawLine(int state)
+{
+    _drawLine = state != 0;
+    settings->setValue("MetaballsView/drawLine", _drawLine);
+
+    lineSizeSlider->setEnabled(_drawLine);
+    pointSizeSlider->setDisabled(_drawLine);
+}//setDrawLine
+
+void QuantiseErrorView::setPointSize(int value)
+{
+    if (value > MAX_POINT_SIZE) value = MAX_POINT_SIZE;
+    if (value < 1) value = 1;
+    _pointSize = value;
+    if (current) glPointSize(_pointSize);
+
+    settings->setValue("QuantiseErrorView/pointSize", _pointSize);
+}//setPointSize
 
 void QuantiseErrorView::setLineSize(int value)
 {
@@ -198,6 +287,7 @@ void QuantiseErrorView::setLineSize(int value)
 void QuantiseErrorView::setStartErrorValue(double value)
 {
     _startErrorValue = value;
+    finalErrorSpinBox->setMinimum(_startErrorValue);
 
     settings->setValue("QuantiseErrorView/startError", _startErrorValue);
     tick(framenum, unquantised, quantised, dequantised);
@@ -206,6 +296,7 @@ void QuantiseErrorView::setStartErrorValue(double value)
 void QuantiseErrorView::setFinalErrorValue(double value)
 {
     _finalErrorValue = value;
+    startErrorSpinBox->setMaximum(_finalErrorValue);
 
     settings->setValue("QuantiseErrorView/finalError", _finalErrorValue);
     tick(framenum, unquantised, quantised, dequantised);
@@ -246,4 +337,14 @@ void QuantiseErrorView::setFinalAlpha(int value)
 
     settings->setValue("QuantiseErrorView/finalColorA", _finalColor[3]);
 }//setFinalAlpha
+
+void QuantiseErrorView::setThresholdValue(int value)
+{
+    if (value < 0) value = 0;
+    if (value > 100) value = 100;
+    _thresholdValue = value / 100.0;
+
+    settings->setValue("QuantiseErrorView/thresholdValue", _thresholdValue);
+    tick(framenum, unquantised, quantised, dequantised);
+}//setThresholdValue
 
